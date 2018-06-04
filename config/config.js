d6639c0f3df3bbf072afc211223d8563187764d5
yehiyam <yehiyam@gmail.com>
2018-06-04 18:11:38 +0300

    fix elastic test

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const config = {
     // apiServerUrl: process.env.API_URL || 'https://40.69.222.75/hkube/api-server/api/v1',
     apiServerUrl: process.env.API_URL || 'https://10.32.10.27/hkube/api-server/api/v1',
+    elasticsearchUrl: process.env.ELASTICSEARCH_URL || 'https://10.32.10.27/system/elasticsearch',
     reject_selfSigned: false
 }
 if (!config.reject_selfSigned) {
--- utils/results.js
+++ utils/results.js
@@ -1,17 +1,19 @@
 const chai = require('chai');
 const expect = chai.expect;
+const delay = require('delay');
 const config = require('../config/config');
 
 
-const getResult = async (jobId, expectedStatus, timeout=60000) => {
+const getResult = async (jobId, expectedStatus, timeout=60000, interval=1000) => {
     const start = Date.now();
     do {
+        process.stdout.write('.')
         const res = await chai.request(config.apiServerUrl)
             .get(`/exec/results/${jobId}`);
         if (res.status==expectedStatus){
             return res.body;
         }
-        // console.log(res.error)
+        await delay(interval);
     } while (Date.now() - start < timeout);
     expect.fail(`timeout exceeded trying to get ${expectedStatus} status for jobId ${jobId}`);
 };
